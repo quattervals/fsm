352eff7444c033905bc74a12683944bb2f0007c2
Oliver With <oliverwith@bbv.ch>
2025-08-19 16:24:36 +0200

    Organize into modules

---
+++ src/machines/lathe.rs
@@ -0,0 +1,91 @@
+use std::marker::PhantomData;
+
+#[derive(Debug)]
+pub struct Off;
+#[derive(Debug)]
+pub struct Spinning;
+#[derive(Debug)]
+pub struct Feeding;
+#[derive(Debug)]
+pub struct Notaus;
+
+#[derive(Default, Debug)]
+pub struct LatheData {
+    revs: u32,
+    feed: u32,
+}
+
+#[derive(Debug)]
+pub struct Lathe<State> {
+    state: PhantomData<State>,
+    business_data: Box<LatheData>,
+}
+
+impl<State> Lathe<State> {
+    pub fn new(data: Box<LatheData>) -> Lathe<Off> {
+        Lathe {
+            state: PhantomData,
+            business_data: data,
+        }
+    }
+
+    pub fn notaus(self) -> Lathe<Notaus> {
+        Lathe {
+            state: PhantomData,
+            business_data: self.business_data,
+        }
+    }
+
+    pub fn print(&self) {
+        println!("State {:?}, Data {:#?}", self.state, self.business_data)
+    }
+}
+
+impl Lathe<Off> {
+    pub fn start_spinning(mut self, revs: u32) -> Lathe<Spinning> {
+        self.business_data.revs = revs;
+        Lathe {
+            state: PhantomData,
+            business_data: self.business_data,
+        }
+    }
+}
+
+impl Lathe<Spinning> {
+    pub fn feed(mut self, feed: u32) -> Lathe<Feeding> {
+        self.business_data.feed = feed;
+
+        Lathe {
+            state: PhantomData,
+            business_data: self.business_data,
+        }
+    }
+    pub fn off(mut self) -> Lathe<Off> {
+        self.business_data = Default::default();
+        Lathe {
+            state: PhantomData,
+            business_data: self.business_data,
+        }
+    }
+}
+
+impl Lathe<Feeding> {
+    pub fn stop_feed(mut self) -> Lathe<Spinning> {
+        self.business_data.feed = 0;
+
+        Lathe {
+            state: PhantomData,
+            business_data: self.business_data,
+        }
+    }
+}
+
+impl Lathe<Notaus> {
+    pub fn quittieren(mut self) -> Lathe<Off> {
+        self.business_data = Default::default();
+        Lathe {
+            state: PhantomData,
+            business_data: self.business_data,
+        }
+    }
+}
