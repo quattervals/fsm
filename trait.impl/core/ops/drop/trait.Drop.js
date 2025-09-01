91c4738cfb0ea8664c7cf8f2a0c2e9850795335c
quattervals <quattervals@users.noreply.github.com>
2025-09-01 15:21:23 +0000

    deploy: 084619d53322f0c10f9b2ca79d83ade3560c6b6d

---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["fsm",[["impl&lt;Command, Response&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"fsm/machines/shared/struct.MachineController.html\" title=\"struct fsm::machines::shared::MachineController\">MachineController</a>&lt;Command, Response&gt;<div class=\"where\">where\n    Command: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> + 'static,\n    Response: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> + 'static,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[718]}
